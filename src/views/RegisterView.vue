f342e8166e55fe2b5b472ac555f79e6d62eb7e82
topongo <lorenzo.bodini.private@gmail.com>
2023-06-07 11:51:17 +0200

    registration form

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,18 +3,21 @@
      <h1>Registration</h1>
       <form @submit.prevent="register">
         <div>
+          <label for="username">Username</label>
+          <input type="text" id="username" v-model="form.username" />
+        </div>
+        <div>
           <label for="email">Email</label>
-          <input type="email" id="email" v-model="email" />
+          <input type="text" id="email" v-model="form.email" />
         </div>
         <div>
           <label for="password">Password</label>
-          <input type="password" id="password" v-model="password" />
+          <input type="password" id="password" v-model.lazy="form.password" />
         </div>
         <div>
           <label for="confirmPassword">Confirm Password</label>
-          <input type="password" id="confirmPassword" v-model="confirmPassword" />
+          <input type="password" id="confirmPassword" v-model.lazy="form.confirm_password" />
         </div>
-        <pre @submit="(f) => form = f">{{ form }}</pre>
         <input type="submit" value="Register" />
       </form>
   </div>
@@ -22,23 +25,44 @@
 
 <script>
 export default {
-  // data() {
-  //   return {
-  //     form: {
-  //       email: ,
-  //       password: '',
-  //       confirmPassword: ''
-  //     }
-  //   }
-  // },
-  methods: {
-    register(submitEvent) {
-      this.data = submitEvent
-    },
-    submit() {
-      console.log(this.data)
-      this.$emit('submit', this.data_)
+  data() {
+    console.log("queried data!")
+    return {
+      form: {
+        email: '',
+        password: '',
+        confirm_password: ''
+      },
     }
+  },
+  methods: {
+    async register(submitEvent) {
+      this.$emit('submit', this.form)
+      console.log(this.form.password, this.form.confirm_password)
+      if(this.form.password != this.form.confirm_password) {
+        alert("Passwords do not match!")
+        return
+      }
+      console.log(JSON.stringify({
+          username: this.form.username,
+          password: this.form.password,
+          email: this.form.email
+        })
+        )
+      console.log(this.form)
+      console.log(await fetch("http://127.0.0.1:8080/api/v1/auth/register", {
+        method: "POST",
+        headers: {
+          "Host": "127.0.0.1:8080",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: this.form.username,
+          password: this.form.password,
+          email: this.form.email
+        }),
+      }).then((res) => res.json()))
+    },
   }
 }
 </script>
